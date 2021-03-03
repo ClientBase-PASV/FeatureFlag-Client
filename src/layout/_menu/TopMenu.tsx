e09e563c115781b0edf9712e177e39204eec2899
Viktor Bogutskii <info@siteograf.com>
2021-03-03 14:17:11 -0500

    update top menu

--- src/layout/_menu/TopMenu.tsx
+++ src/layout/_menu/TopMenu.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { connect, Link, withRouter } from 'umi';
 import { get } from 'lodash';
-// @ts-ignore
 import classNames from 'classnames';
 import { IUserAccount } from '@/pages/user/userSearch/types';
-import { Dropdown, Menu } from 'antd';
-import dotsIcon from '@/icons/dots-horizontal.svg';
 
 interface IProps {
   Account: IUserAccount;
@@ -14,79 +11,22 @@ interface IProps {
 const TopMenu = (props: IProps) => {
   const location = get(props, 'location.pathname', '');
   const acl = get(props, 'Account.acl', []);
-  const userCompanyAccountId = get(props, 'Account.companyAccount', '');
 
-  const guestMenu = [
-    { path: '/pricing', name: 'Pricing' },
-    { path: '/industries', name: 'Industries' },
-    { path: '/support', name: 'Support' },
-  ].map((el) => ({
-    ...el,
-    isActive: location.startsWith(el.path),
-  }));
-
-  const mainMenu = [
-    { path: '/client', name: 'Clients', perm: 'client.get.own' },
-    { path: '/order', name: 'Orders', perm: 'order.get.own' },
-    { path: '/invoice', name: 'Invoices', perm: 'invoice.get.own' },
-    { path: '/estimate', name: 'Estimates', perm: 'estimate.get.own' },
-    { path: '/schedule', name: 'Schedule', perm: 'order.get.own' },
-    { path: '/payment', name: 'Payments', perm: 'payment.get.own' },
-  ].map((el) => ({
+  const mainMenu = [{ path: '/base', name: 'Base', perm: 'base.get.own' }].map((el) => ({
     ...el,
     isActive: location.startsWith(el.path),
     isVisible: acl.includes(el.perm),
   }));
-
-  const secondaryMenu = [
-    { path: '/address', name: 'Addresses', perm: 'address.get.own' },
-    { path: '/vendor', name: 'Vendors', perm: 'vendor.get.own' },
-    { path: '/product', name: 'Products', perm: 'product.get.own' },
-    { path: '/purchaseOrder', name: 'Purchase Orders', perm: 'purchaseOrder.get.own' },
-  ].map((el) => ({
-    ...el,
-    isActive: location.startsWith(el.path),
-    isVisible: acl.includes(el.perm),
-  }));
-
-  const secondaryMenuComponent = (
-    <Menu>
-      {secondaryMenu.map(
-        (el) =>
-          el.isVisible && (
-            <Menu.Item key={el.path}>
-              <Link to={el.path}>{el.name}</Link>
-            </Menu.Item>
-          ),
-      )}
-    </Menu>
-  );
 
   return (
     <div id="top-menu" role="menu" className="d-flex d-print-none">
-      {userCompanyAccountId ? (
-        <>
-          {mainMenu.map(
-            (el) =>
-              el.isVisible && (
-                <div className={classNames('item', { active: el.isActive })} key={el.path}>
-                  <Link to={el.path}>{el.name}</Link>
-                </div>
-              ),
-          )}
-
-          <Dropdown overlay={secondaryMenuComponent}>
-            <span className="item ant-dropdown-link pointer">
-              <img src={dotsIcon} alt="" height="27" />
-            </span>
-          </Dropdown>
-        </>
-      ) : (
-        guestMenu.map((el) => (
-          <div className={classNames('item', { active: el.isActive })} key={el.path}>
-            <Link to={el.path}>{el.name}</Link>
-          </div>
-        ))
+      {mainMenu.map(
+        (el) =>
+          el.isVisible && (
+            <div className={classNames('item', { active: el.isActive })} key={el.path}>
+              <Link to={el.path}>{el.name}</Link>
+            </div>
+          ),
       )}
     </div>
   );
